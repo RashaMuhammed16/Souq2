d97cff2f70784da33026b6aaa8ae9e8148164574
“alaa <alaasalaheddin013@gmail.com>
2021-06-08 18:52:41 +0200

    Adding Sub_Category

--- Api/Controllers/Sub_CategoryController.cs
+++ Api/Controllers/Sub_CategoryController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BL.AppServices;
+using BL.ViewModel;
 
 namespace Api.Controllers
 {
@@ -11,5 +13,78 @@ namespace Api.Controllers
     [ApiController]
     public class Sub_CategoryController : ControllerBase
     {
+        Sub_CategortAppService sub_CategortAppService;
+
+        public Sub_CategoryController(Sub_CategortAppService  sub_CategortAppService)
+        {
+            this.sub_CategortAppService = sub_CategortAppService;
+        }
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            return Ok(sub_CategortAppService.GetAll());
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetSubCategoryById(int id)
+        {
+            return Ok(sub_CategortAppService.GetById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Create(Sub_CategoryViewModel subcategoryViewModel)
+        {
+
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                sub_CategortAppService.SaveNewSubCategory(subcategoryViewModel);
+
+                //string urlDetails = Url.Link("DefaultApi", new { id = categoryViewModel.ID });
+                //return Created(urlDetails, "Added Sucess");
+                return Created("CreateCategory", subcategoryViewModel);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(int id, Sub_CategoryViewModel SubcategoryViewModel)
+        {
+
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                sub_CategortAppService.UpdateCategory(SubcategoryViewModel);
+                return Ok(SubcategoryViewModel);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Sub_CategoryViewModel sub_CategoryViewModel = sub_CategortAppService.GetById(id);
+            try
+            {
+                sub_CategortAppService.DeleteSubCategory(sub_CategoryViewModel);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
--- BL/AppServices/Sub_CategortAppService.cs
+++ BL/AppServices/Sub_CategortAppService.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace BL.AppServices
 {
-    class Sub_CategortAppService: BaseAppService
+  public class Sub_CategortAppService: BaseAppService
     {
         public Sub_CategortAppService(IUnitOfWork theUnitOfWork, IMapper mapper) : base(theUnitOfWork, mapper)
         {
